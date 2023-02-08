7de0e6d08499219f7cc6dbe0c2fe924b1fa098d1
c332030 <c332030@gmail.com>
2023-02-09 00:43:18 +0800

    feat: build

--- packages/c-common/build.js
+++ packages/c-common/build.js
@@ -1,14 +1,16 @@
 
 const fs = require('fs')
 
-fs.rm('dist', { recursive:true }, (err) => {
+const build = 'build'
+
+fs.rm(build, { recursive:true }, (err) => {
 
   // 忽略文件不存在报错
   if(err?.message?.indexOf('no such file or directory') < 0) {
     process.exit(-1)
   }
 
-  fs.cp('src/css/', 'dist/css', {recursive: true}, (err) => {
+  fs.cp('src/css/',  `${build}/css`, {recursive: true}, (err) => {
 
     if(err){
       console.error(err.message)
